42c43a3e79464dbd60c30a7ecc789bc1feb3cbfb
Akhil-Techsoft <techsofttest@gmail.com>
2026-07-14 15:09:21 +0530

    Fix unique constraint for coupons

---
+++ database/migrations/2026_07_14_093747_remove_unique_from_coupon_amount_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('coupons', function (Blueprint $table) {
+            $table->dropUnique('tc_coupons_coupon_amount_unique');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('coupons', function (Blueprint $table) {
+            $table->unique('coupon_amount', 'tc_coupons_coupon_amount_unique');
+        });
+    }
+};
