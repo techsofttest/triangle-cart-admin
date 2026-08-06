e838b317b471b882528cc4dc6c56a90b46a29c6c
Akhil-Techsoft <techsofttest@gmail.com>
2026-08-06 14:21:54 +0530

    Fix admin session

--- app/Http/Middleware/SetSessionForAdmin.php
+++ app/Http/Middleware/SetSessionForAdmin.php
@@ -15,9 +15,20 @@ class SetSessionForAdmin
         $host = $request->getHost();
 
         if (Str::contains($host, 'admin')) {
+            $sessionDomain = env('ADMIN_SESSION_DOMAIN');
+            if ($sessionDomain === 'null') {
+                $sessionDomain = null;
+            } elseif ($sessionDomain) {
+                // If current host environment does not match configured domain (e.g. test vs com.au), default to null
+                $configBase = Str::after($sessionDomain, 'admin.');
+                if (!Str::contains($host, $configBase)) {
+                    $sessionDomain = null;
+                }
+            }
+
             config([
                 'session.cookie' => env('ADMIN_SESSION_COOKIE', 'admin_session'),
-                'session.domain' => env('ADMIN_SESSION_DOMAIN', $request->getHost()),
+                'session.domain' => $sessionDomain,
                 'session.same_site' => env('ADMIN_SESSION_SAME_SITE', config('session.same_site')),
             ]);
         }
